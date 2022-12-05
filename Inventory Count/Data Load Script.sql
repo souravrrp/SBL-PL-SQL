e1bf233d87afa0a16390d1e3df113be9ff32ad14
sourav.paul <sourav.paul@singerbd.net>
2022-12-05 15:51:45 +0600

    051222

---
+++ Inventory Count/Data Load Script.sql
@@ -0,0 +1,25 @@
+-- Data Load 246 & in-transit
+--insert into SBL_INVENTORY_COUNTING_DTS
+--  (CATALOG_NO,
+--   SITE,
+--   QTY_ONHAND,
+--   REVERT_REV,
+--   REVERT_BAL,
+--   QTY_IN_TRANSIT)
+--  select t.part_no,
+--         t.contract,
+--         t.cf_balance,
+--         t.revert_rev,
+--         t.revert_bal,
+--         c.in_transit
+--    from ifsapp.REP246_TAB t
+--   inner join ifsapp.INVENT_ONLINE_COST_TAB c
+--      on t.stat_year = c.year
+--     and t.stat_period_no = c.period
+--     and t.contract = c.contract
+--     and t.part_no = c.part_no
+--   where t.stat_year = '&year_i'
+--     and t.stat_period_no = '&period'
+--     and t.cf_balance > 0
+--   order by t.contract, t.part_no;
+--commit;
---
+++ Report/New Jasper/Marketing Pending PO With Order/Corporate Pending PO With Order.sql
@@ -0,0 +1,101 @@
+/* Formatted on 11/16/2022 3:38:31 PM (QP5 v5.381) */
+-- CS Pending PO
+SELECT channel,
+       order_number,
+       po_no,
+       TRUNC (po_date)                po_date,
+       order_from                     order_site,
+       order_to                       delivery_site,
+       sku                            part_no,
+       (SELECT product_family
+          FROM ifsapp.sbl_jr_product_dtl_info
+         WHERE product_code = sku)    product_family,
+       po_quantity                    order_quantity,
+       receive_quantity               delivery_quantity,
+       pending_quantity,
+       po_status
+  FROM (SELECT pot.order_no
+                   po_no,
+               (CASE
+                    WHEN pot.contract IN ('JWSS',
+                                   'SAOS',
+                                   'SWSS',
+                                   'WSMO',
+                                   'WITM',
+                                   'SITM',
+                                   'SSAM')
+                    THEN
+                        'WHOLESALE'
+                    WHEN pot.contract = 'SCSM'
+                    THEN
+                        'CORPORATE'
+                    WHEN pot.contract = 'SOSM'
+                    THEN
+                        'ONLINE CHANNEL'
+                    WHEN pot.contract IN ('SAPM',
+                                   'SESM',
+                                   'SHOM',
+                                   'SISM',
+                                   'SFSM')
+                    THEN
+                        'STAFF SCRAP AND ACQUISITION'
+                    ELSE
+                        'RETAIL'
+                END)
+                   channel,
+               pot.contract
+                   order_from,
+               pot.vendor_no
+                   order_to,
+               polt.part_no
+                   sku,
+               polt.buy_qty_due
+                   po_quantity,
+               NVL (rr.qty_arrived, 0)
+                   receive_quantity,
+               (polt.buy_qty_due - NVL (rr.qty_arrived, 0))
+                   pending_quantity,
+               TRUNC (pot.order_date)
+                   po_date,
+               pot.rowstate
+                   po_status,
+               copo.oe_order_no
+                   order_number
+          FROM ifsapp.purchase_order_tab        pot,
+               ifsapp.purchase_order_line_tab   polt,
+               ifsapp.customer_order_pur_order  copo,
+               (  SELECT r.order_no,
+                         r.line_no,
+                         r.release_no,
+                         r.part_no,
+                         SUM (r.qty_arrived)     qty_arrived
+                    FROM ifsapp.purchase_receipt_new r
+                   WHERE r.state = 'Received'
+                GROUP BY r.order_no,
+                         r.line_no,
+                         r.release_no,
+                         r.part_no) rr
+         WHERE     pot.order_no = polt.order_no
+               AND pot.rowstate NOT IN ('Cancelled', 'Closed')
+               AND polt.rowstate NOT IN ('Cancelled', 'Closed')
+               AND order_sent = 'SENT'
+               AND pot.contract = 'SCSM'
+               AND pot.vendor_no = :p_warehouse
+               AND polt.order_no = copo.po_order_no
+               AND polt.line_no = copo.po_line_no
+               AND polt.release_no = copo.po_rel_no
+               AND EXISTS
+                       (SELECT 1
+                          FROM ifsapp.customer_order co
+                         WHERE     1 = 1
+                               AND co.customer_po_no = pot.order_no
+                               AND co.state NOT IN
+                                       ('Cancelled',
+                                        'Closed',
+                                        'Invoiced/Closed'))
+               AND polt.order_no = rr.order_no(+)
+               AND polt.line_no = rr.line_no(+)
+               AND polt.release_no = rr.release_no(+)
+               AND polt.part_no = rr.part_no(+))
+ WHERE pending_quantity > 0
+ and rownum<=3;
